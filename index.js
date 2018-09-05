f9080ade48966aac395340c352106c464f3eed7c
Andrew Cherabaev <a.cherabaev@php73.ru>
2018-09-05 07:57:21 +0300

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-var util = require('util');
-var {DataBase} = require('./lib/database');
+var DataBase = require('./lib/database');
+var Store = require('./lib/store');
 
-var database = new DataBase('storage', [
-	'users'
-]);
-
-var users = database.get('users');
-
-users.save({id: 1, name: 'username'})
-	 .save({id: 2, name: 'not username'})
-	 .save({id: 3, name: 'also username'});
-
-console.log(users.find({id: 2, name: 'not username'}));
-console.log(users.find(1));
-console.log(users.findFirst(1));
-
-database.drop();
+module.exports = {
+	DataBase,
+	Store
+}
