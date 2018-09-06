40a47f0e7b71786aafc20abebf255c2361353fbc
Andrew Cherabaev <a.cherabaev@php73.ru>
2018-09-06 19:03:33 +0300

    Rename typeof->underscore, add drop(), add index blueprint, fixes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jstorm
 JavaScript file storage and ORM
+
+#todo
+add class `index`
+add index to `store`
+`find()` calls `indexed` and `nonindexed` depends on existing index
--- helpers/underscore.js
+++ helpers/underscore.js
@@ -1,3 +1,9 @@
+const _upFirst = function(arg) {
+	const first = arg[0];
+
+	return first.toUpperCase() + arg.substring(1);
+}
+
 const _typeof = function(arg) {
     return toString.call(arg).match(/^\[object (\w+)\]$/)[1].toLowerCase();
 }
@@ -6,11 +12,16 @@ const _instanceof = function(arg) {
     try {
         return arg.constructor.name;
     } catch (e) {
-        return "Undefined";
+        return _pipe(arg, _typeof, _upFirst);
     }
+}
+
+const _pipe = function(...args){
+	return args.reduce((a, b) => b(a));
 }
 
 module.exports = {
 	_typeof,
-	_instanceof
+	_instanceof,
+	_pipe
 }
