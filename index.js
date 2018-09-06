ab0f4cedb47bc6b6aeea4074dc548d2531cc5d07
Andrew Cherabaev <a.cherabaev@php73.ru>
2018-09-06 19:04:37 +0300

    why i always forget to remove test data?

--- index.js
+++ index.js
@@ -6,20 +6,3 @@ module.exports = {
 	DataBase,
 	Store
 };
-
-const database = new DataBase('storage', [
-	'users'
-]).useIndexes();
-
-const users = database.get('users');
-
-users.save({id: 1, name: 'username'})
-	 .save({id: 2, name: 'not username'})
-	 .save({id: 3, name: 'also username'});
-
-console.log(users.find({id: 2, name: 'not username'}));
-console.log(users.find(1));
-console.log(users.findFirst(1));
-console.log(_instanceof(null), _typeof(null));
-
-// users.drop() == database.dropTable('users');
